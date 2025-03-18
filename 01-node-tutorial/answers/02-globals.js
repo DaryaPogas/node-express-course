3b93663f9d23cb5ba58a5b0bfaea8119ee9da9b2
Darya <daryapogas@gmail.com>
2025-03-18 14:25:15 -0500

    answers for lesson1. for q12 - just playing around a bit - did not complet yet

---
+++ 01-node-tutorial/answers/02-globals.js
@@ -0,0 +1,5 @@
+console.log(__dirname)
+console.log(__filename)
+console.log('MY_VAR:',process.env.MY_VAR);
+
+//Set an environment variable with the following command in your command line terminal: export MY_VAR=Hi_There
